c8db1fd9e09802963f5e2b537221ea339bf8c969
Fox <fxcourel@gmail.com>
2022-02-28 11:57:55 +0100

    Implemented Theme Switch

--- js/main.js
+++ js/main.js
@@ -1,5 +1,38 @@
+const page = document.querySelector('html');
+const themeBtn = document.querySelector('#theme-switcher');
+
+function toggleTheme(theme=null) {
+    if(theme === 'light'){
+        page.classList.remove('dark');
+        page.classList.add('light');
+        localStorage.theme = 'light'
+    }
+    else if(theme === 'dark'){
+        page.classList.remove('light');
+        page.classList.add('dark');
+        localStorage.theme = 'dark'
+    }
+    else{
+        if(localStorage.theme === 'dark'){
+            toggleTheme('light');
+            return
+        }
+        else if(localStorage.theme === 'light'){
+            toggleTheme('dark');
+            return
+        }
+    }
+}
+
 function init() {
     console.debug("Initialized JS");
+
+    // Initialize theme
+    if(localStorage.theme){
+        toggleTheme(localStorage.theme);
+    } else {
+        toggleTheme('dark');
+    }
 }
 
 init();
